214ee123f60f992d4873abbf68427e9abef4f6f8
suhaib <suhaibvbm@gmail.com>
2023-11-04 09:47:16 +0530

    file reding using 'with' keyword

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-# there is a method called open ( inbuilt method )
-file = open("new_file.txt")
-# read method return content of the file as a string
-contents = file.read()
-print(contents)
-# we have to close the file, it takes up some resources of computer
-file.close()
+# # there is a method called open ( inbuilt method )
+# file = open("new_file.txt")
+# # read method return content of the file as a string
+# contents = file.read()
+# print(contents)
+# # we have to close the file, it takes up some resources of computer
+# file.close()
+
+### it hard to remember close the file
+
+# with keyword
+with open("new_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+
+
