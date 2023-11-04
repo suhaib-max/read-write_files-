ecb9f5903e3192e8273cfe90e1cac890955664fa
suhaib <suhaibvbm@gmail.com>
2023-11-04 09:41:56 +0530

    file opening and reading closing

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+# there is a method called open ( inbuilt method )
+file = open("new_file.txt")
+# read method return content of the file as a string
+contents = file.read()
+print(contents)
+# we have to close the file, it takes up some resources of computer
+file.close()
